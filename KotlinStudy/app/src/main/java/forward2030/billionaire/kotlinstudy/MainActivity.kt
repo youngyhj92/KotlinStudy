e5c266224bfdb4d670f9fe53553bd03e0d6c8cf1
youngyhj92 <makellan1926@gmail.com>
2018-09-22 23:25:41 +0900

    recyclerView LinearLayoutManager study commit

--- KotlinStudy/app/src/main/java/forward2030/billionaire/kotlinstudy/MainActivity.kt
+++ KotlinStudy/app/src/main/java/forward2030/billionaire/kotlinstudy/MainActivity.kt
@@ -2,12 +2,13 @@ package forward2030.billionaire.kotlinstudy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,56 +18,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         howlRecyclerView.adapter = MainRecyclerViewAdapter()
-        var gridManager= GridLayoutManager(this, 3)
-        howlRecyclerView.layoutManager =gridManager
-        
-        gridManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup()  {
-            override fun getSpanSize(p0: Int): Int {
-                if(p0 == 0)  {
-                    return 3
-                }
-                return 1
-            }
-
-        }
-
-
+        howlRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     class MainRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()  {
         var images = arrayOf(R.drawable.test, R.drawable.test2, R.drawable.test3, R.drawable.test4)
+        var titles = arrayOf("프랑크프루트", "런던", "파리", "서울")
+        var countries = arrayOf("독일", "영국", "프랑스", "한국")
 
         override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(p0!!.context).inflate(R.layout.item_layout,p0,false)
-            var params = view.layoutParams
+            var view = LayoutInflater.from(p0!!.context).inflate(R.layout.item_layout, p0,false)
 
-            if(p1 == 0)  {
-                params.height = (p0.measuredWidth / 3) * 2
-                params.width = p0.measuredWidth
-                view.layoutParams = params
-                return CustomViewHolder(view)
-            }
-            params.height = p0.measuredWidth / 3
-            params.width = p0.measuredWidth / 3
-            view.layoutParams = params
             return CustomViewHolder(view)
-
         }
 
         class CustomViewHolder(view: View?) : RecyclerView.ViewHolder(view!!) {
-            var imageView : ImageView? = null
+            var imageView:ImageView? = null
+            var textView : TextView? = null
+            var textView2 : TextView? = null
             init {
-                imageView = view!!.findViewById(R.id.layoutItem_imageView)
+                imageView = view?.findViewById(R.id.profileImageView)
+                textView = view?.findViewById(R.id.title_TextView)
+                textView2 = view?.findViewById(R.id.country_textView)
             }
 
-        }
-
-        // 포지션 값에 따라서 타입을 바꿔주는 기능. - onCreateViewHolder
-        override fun getItemViewType(position: Int): Int {
-            if(position == 0)  {
-                return 0
-            }
-            return 1
         }
 
         override fun getItemCount(): Int {
@@ -75,9 +50,10 @@ class MainActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
             var view = p0 as CustomViewHolder
-            view!!.imageView!!.setImageResource(images[p1])
+            view.imageView?.setImageResource(images[p1])
+            view.textView?.text = titles[p1]
+            view.textView2?.text = countries[p1]
         }
-
 
     }
 }
